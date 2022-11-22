f640c0f43b0f8922a2212d061123f8ae7edb93d2
BAEKSEONGYUN <baek@nsmarts.co.kr>
2022-11-22 12:34:56 +0900

    model layer change

--- siamese_nn2.py
+++ siamese_nn2.py
@@ -179,12 +179,12 @@ tower_2 = embedding_network(input_2)
 merge_layer = layers.Lambda(utils_test.euclidean_distance)([tower_1, tower_2])
 model = keras.Model(inputs=[input_1, input_2], outputs=merge_layer)
 
-normal_layer = tf.keras.layers.BatchNormalization()(merge_layer)
-output_layer = layers.Dense(1, activation="sigmoid")(normal_layer)
-model = keras.Model(inputs=[input_1, input_2], outputs=output_layer)
+# normal_layer = tf.keras.layers.BatchNormalization()(merge_layer)
+# output_layer = layers.Dense(1, activation="sigmoid")(normal_layer)
+# model = keras.Model(inputs=[input_1, input_2], outputs=output_layer)
 
-adam = optimizers.Adam(lr=0.00005)
-adam = optimizers.adam_v2(lr=0.00005)
+# adam = optimizers.Adam(lr=0.00005)
+# adam = optimizers.adam_v2(lr=0.00005)
 
 # opt = tf.keras.optimizers.Adam(learning_rate=0.0005, beta_1=0.5)
 opt = tf.keras.optimizers.Adam(learning_rate=0.0005, beta_1=0.5)
