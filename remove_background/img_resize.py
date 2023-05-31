f71593ce9d5ed6a0b46e7264825cfb35f5955e7c
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-05-31 16:58:52 +0900

    new img resize code create

---
+++ remove_background/img_resize.py
@@ -0,0 +1,32 @@
+import cv2
+from PIL import Image
+import numpy as np
+###############################################################
+
+img = cv2.imread("myFace1.png")
+
+print("before : ", img.shape)
+
+resized_img = cv2.resize(img, (106,106,2), interpolation=cv2.INTER_AREA)
+
+print("after : ", img.shape)
+
+##############################################################
+
+# 이미지 읽기
+img = Image.open("myFace1.png")
+
+# 이미지 크기 출력
+print("Original image size:", img.size)
+
+# 이미지 크기 변경
+resized_img = img.resize((106, 106), resample=Image.BICUBIC)
+
+# 변경된 이미지 크기 출력
+print("Resized image size:", resized_img.size)
+
+image_array = np.array(resized_img)
+
+print("after resized_img : ", image_array.shape)
+
+cv2.imwrite('aaa.png', image_array)
