0ea187331528b82f14db8261a4e428977f1b5531
ljwoon <ljwoon@nsmarts.co.kr>
2023-10-17 18:36:09 +0900

    [Update] data model config

--- new_detection_face/tinface_r50.py
+++ new_detection_face/tinface_r50.py
@@ -78,7 +78,7 @@ model = dict(
         dict(
             typename='FPN',
             in_channels=[256, 512, 1024, 2048],
-            out_channels=256,
+            out_channels=512,
             start_level=0,
             add_extra_convs='on_input',
             num_outs=6,
@@ -86,7 +86,7 @@ model = dict(
             upsample_cfg=dict(mode='bilinear')),
         dict(
             typename='Inception',
-            in_channel=256,
+            in_channel=512,
             num_levels=6,
             norm_cfg=dict(typename='BN'),
             share=True)
@@ -95,9 +95,9 @@ model = dict(
         typename='IoUAwareRetinaHead',
         num_classes=num_classes,
         num_anchors=num_anchors,
-        in_channels=256,
+        in_channels=512,
         stacked_convs=4,
-        feat_channels=256,
+        feat_channels=512,
         norm_cfg=dict(typename='BN'),
         use_sigmoid=use_sigmoid))
 
