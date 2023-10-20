4d6e84bfe65be468132198e1408d0445939ab62f
ljwoon <ljwoon@nsmarts.co.kr>
2023-10-20 18:24:53 +0900

    [Debug] remove_background

--- remove_background/remove_backgroun.py
+++ remove_background/remove_backgroun.py
@@ -10,20 +10,20 @@ from rembg import remove
 # print('imread')
 img_imread = cv2.imread('client_face1.png')
 
-# output = Image.open('client_face1.png')
+output = Image.open('client_face1.png')
 output = remove(img_imread)
 # output.save('zzz2.png')
 
 cv2.imwrite('zzz222.png', output)
 
-# cv2.imshow('11', img_imread)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('11', img_imread)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-# output = remove(img_imread)
+output = remove(img_imread)
 
 
-# img_pil = Image.open('removal_background\client_face1.png')
+img_pil = Image.open('removal_background\client_face1.png')
 
-# print('imwrite')
-# cv2.imwrite('out.png', output)
+print('imwrite')
+cv2.imwrite('out.png', output)
